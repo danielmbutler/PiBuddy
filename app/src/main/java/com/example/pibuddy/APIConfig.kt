ac8ff42bc92709ec892f56fa8cd1a106f828a211
danielmbutler <danielmbutler@hotmail.co.uk>
2021-01-02 19:19:08 +0000

    add ping check - app complete

--- app/src/main/java/com/example/pibuddy/APIConfig.kt
+++ app/src/main/java/com/example/pibuddy/APIConfig.kt
@@ -1,5 +1,5 @@
 package com.example.pibuddy
 
 public class APIConfig {
-    public val Lambda  = "https://0c110sapg5.execute-api.us-east-1.amazonaws.com/default/pidatafetcher"
+    public val Lambda  = "api"
 }
